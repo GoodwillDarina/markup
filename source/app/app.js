9d11b8a66901a5cf0b9ba24251ca72fca6a75d70
Darya Saburova <saburova@azoft.com>
2017-11-27 18:05:40 +0700

    fixed coords for border

--- source/app/app.js
+++ source/app/app.js
@@ -42,7 +42,11 @@ app.controller('landingCtrl', [
 
 app.directive('draggable', ['$document', function($document) {
     return function(scope, element, attr) {
-        var mouseOffset, newOffset;
+        var mouseOffset;
+        var newOffset = {
+            x: null,
+            y: null
+        };
         var xPos, yPos;
         var areaDrag = element.parent();
         var areaPosition = {
@@ -74,21 +78,25 @@ app.directive('draggable', ['$document', function($document) {
             xPos = event.pageX - mouseOffset.x - areaPosition.x;
             yPos = event.pageY - mouseOffset.y - areaPosition.y;
 
-            if(yPos <= 0 || xPos <= 0) {
-                newOffset = {
-                    y: yPos <= 0 ? 0 : yPos,
-                    x: xPos <= 0 ? 0 : xPos
-                };
-            } else if(yPos >= areaPosition.y1 || xPos >= areaPosition.x1){
-                newOffset = {
-                    y: yPos >= areaPosition.y1 ? areaPosition.y1 : yPos,
-                    x: xPos >= areaPosition.x1 ? areaPosition.x1 : xPos
-                };
+            if(yPos >= 0 && yPos <= areaPosition.y1) {
+                newOffset.y = yPos;
             } else {
-                newOffset = {
-                    y: yPos,
-                    x: xPos
-                };
+                if(yPos > areaPosition.y1) {
+                    newOffset.y = areaPosition.y1;
+                }
+                else {
+                    newOffset.y = 0;
+                }
+            }
+            if(xPos >= 0 && xPos <= areaPosition.x1)  {
+                newOffset.x = xPos;
+            } else {
+                if(xPos > areaPosition.x1){
+                    newOffset.x = areaPosition.x1;
+                }
+                else {
+                    newOffset.x = 0;
+                }
             }
 
             element.css({
