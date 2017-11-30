fad0adc8e7f9dc774809b9470ffefe5e0d145e7e
Daria Saburova <ya.goodwill.darina@yandex.ru>
2017-11-30 23:45:23 +0700

    fixed bug for IE11

--- source/app/app.js
+++ source/app/app.js
@@ -42,118 +42,120 @@ app.controller('landingCtrl', [
 
 app.directive('draggable', ['$document', '$window', function($document, $window) {
     return function(scope, element, attr) {
-        var mouseOffset;
-        var newOffset = {
-            x: null,
-            y: null
-        };
-        var xPos, yPos;
+        var ua = window.navigator.userAgent.toLowerCase();
+        var is_ie = (/trident/gi).test(ua) || (/msie/gi).test(ua);
 
-        var areaDrag = element.parent();
-        var areaPosition = {
-            x: areaDrag[0].getBoundingClientRect().x,
-            y: areaDrag[0].getBoundingClientRect().y,
-            x1: areaDrag[0].getBoundingClientRect().width - element[0].getBoundingClientRect().width,
-            y1: areaDrag[0].getBoundingClientRect().height - element[0].getBoundingClientRect().height
-        };
-        var phoneBorder = element.next();
-
-        var burger = phoneBorder.children().children();
-        var burgerPosition = {
-            x: burger[0].getBoundingClientRect().x,
-            y: burger[0].getBoundingClientRect().y
-        };
-
-        function getMouseOffset(target, event) {
-            var elementOffset = getPosition(target);
-
-            return {
-                x: event.pageX - elementOffset.x,
-                y: event.pageY - elementOffset.y
+        if(!is_ie) {
+            var mouseOffset;
+            var newOffset = {
+                x: null,
+                y: null
             };
-        }
+            var xPos, yPos;
 
-        element.on('mousedown', mousedown);
+            var areaDrag = element.parent();
+            var areaPosition = {
+                x: areaDrag[0].getBoundingClientRect().x,
+                y: areaDrag[0].getBoundingClientRect().y,
+                x1: areaDrag[0].getBoundingClientRect().width - element[0].getBoundingClientRect().width,
+                y1: areaDrag[0].getBoundingClientRect().height - element[0].getBoundingClientRect().height
+            };
+            var phoneBorder = element.next();
 
-        function mousedown(event) {
-            event.preventDefault();
+            var burger = phoneBorder.children().children();
+            var burgerPosition = {
+                x: burger[0].getBoundingClientRect().x,
+                y: burger[0].getBoundingClientRect().y
+            };
 
-            mouseOffset = getMouseOffset(element, event);
+            function getMouseOffset(target, event) {
+                var elementOffset = getPosition(target);
 
-            $document.on('mousemove', mousemove);
-            $document.on('mouseup', mouseup);
-            return false;
-        }
-
-        function mousemove(event) {
-            xPos = event.pageX - mouseOffset.x - areaPosition.x;
-            yPos = event.pageY - mouseOffset.y - areaPosition.y;
-
-            if(yPos >= 0 && yPos <= areaPosition.y1) {
-                newOffset.y = yPos;
-            } else {
-                if(yPos > areaPosition.y1) {
-                    newOffset.y = areaPosition.y1;
-                }
-                else {
-                    newOffset.y = 0;
-                }
-            }
-            if(xPos >= 0 && xPos <= areaPosition.x1)  {
-                newOffset.x = xPos;
-            } else {
-                if(xPos > areaPosition.x1){
-                    newOffset.x = areaPosition.x1;
-                }
-                else {
-                    newOffset.x = 0;
-                }
+                return {
+                    x: event.pageX - elementOffset.x,
+                    y: event.pageY - elementOffset.y
+                };
             }
 
-            element.css({
-                top: newOffset.y + 'px',
-                left:  newOffset.x + 'px',
-                bottom: 'auto',
-                right: 'auto'
-            });
-            phoneBorder.css({
-                top: newOffset.y + 'px',
-                left:  newOffset.x + 'px',
-                bottom: 'auto',
-                right: 'auto'
-            });
-            burger.css({
-                top: burgerPosition.y - getElementOffset().y - 80 + 'px',
-                left: burgerPosition.x - getElementOffset().x - 18 + 'px'
-            });
+            element.on('mousedown', mousedown);
 
-            console.warn("mousemove (phoneBorder)", element[0].getBoundingClientRect().x, getElementOffset().x);
-            return false;
-        }
+            function mousedown(event) {
+                event.preventDefault();
 
-        function getElementOffset() {
-            var de = $document[0].documentElement;
-            var box = element[0].getBoundingClientRect();
+                mouseOffset = getMouseOffset(element, event);
+
+                $document.on('mousemove', mousemove);
+                $document.on('mouseup', mouseup);
+                return false;
+            }
+            function mousemove(event) {
+                xPos = event.pageX - mouseOffset.x - areaPosition.x;
+                yPos = event.pageY - mouseOffset.y - areaPosition.y;
+
+                if (yPos >= 0 && yPos <= areaPosition.y1) {
+                    newOffset.y = yPos;
+                } else {
+                    if (yPos > areaPosition.y1) {
+                        newOffset.y = areaPosition.y1;
+                    }
+                    else {
+                        newOffset.y = 0;
+                    }
+                }
+                if (xPos >= 0 && xPos <= areaPosition.x1) {
+                    newOffset.x = xPos;
+                } else {
+                    if (xPos > areaPosition.x1) {
+                        newOffset.x = areaPosition.x1;
+                    }
+                    else {
+                        newOffset.x = 0;
+                    }
+                }
+
+                element.css({
+                    top: newOffset.y + 'px',
+                    left: newOffset.x + 'px',
+                    bottom: 'auto',
+                    right: 'auto'
+                });
+                phoneBorder.css({
+                    top: newOffset.y + 'px',
+                    left: newOffset.x + 'px',
+                    bottom: 'auto',
+                    right: 'auto'
+                });
+                burger.css({
+                    top: burgerPosition.y - getElementOffset().y - 80 + 'px',
+                    left: burgerPosition.x - getElementOffset().x - 18 + 'px'
+                });
+
+                return false;
+            }
+            function mouseup() {
+                $document.off('mousemove', mousemove);
+                $document.off('mouseup', mouseup);
+            }
+
+            function getElementOffset() {
+                var de = $document[0].documentElement;
+                var box = element[0].getBoundingClientRect();
 
 
-            return {
-                y: box.top + $window.pageYOffset - de.clientTop,
-                x: box.left + $window.pageXOffset - de.clientLeft
-            };
-        }
-        function mouseup() {
-            $document.off('mousemove', mousemove);
-            $document.off('mouseup', mouseup);
-        }
+                return {
+                    y: box.top + $window.pageYOffset - de.clientTop,
+                    x: box.left + $window.pageXOffset - de.clientLeft
+                };
+            }
+            function getPosition(elem) {
+                var de = $document[0].documentElement;
+                var box = elem[0].getBoundingClientRect();
 
-        function getPosition(elem) {
-            var de = $document[0].documentElement;
-            var box = elem[0].getBoundingClientRect();
-
-            return {
-                y: box.top + $window.pageYOffset - de.clientTop,
-                x: box.left + $window.pageXOffset - de.clientLeft
-            };
+                return {
+                    y: box.top + $window.pageYOffset - de.clientTop,
+                    x: box.left + $window.pageXOffset - de.clientLeft
+                };
+            }
         }
     };
 }]);
