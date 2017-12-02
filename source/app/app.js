70ca11fffa8893c005a60bc91210033093dc8b04
Daria Saburova <ya.goodwill.darina@yandex.ru>
2017-12-03 01:14:18 +0700

    fixed touch screens and added first animate

--- source/app/app.js
+++ source/app/app.js
@@ -3,7 +3,7 @@ require('angular-scroll');
 require('angular-touch');
 require('ngmodal');
 
-var app = angular.module('ngApp', ['ngAnimate', 'ngModal']);
+var app = angular.module('ngApp', ['ngAnimate', 'ngTouch', 'ngModal']);
 app.controller('landingCtrl', [
     '$scope', '$timeout', function ($scope, $timeout) {
         var isSubmitInProgress = false;
@@ -38,6 +38,10 @@ app.controller('landingCtrl', [
                 $scope.toggleAlert();
             });
         };
+
+        $scope.onTap = function() {
+            console.log(this);
+        }
     }]);
 
 app.directive('draggable', ['$document', '$window', function($document, $window) {
@@ -68,8 +72,10 @@ app.directive('draggable', ['$document', '$window', function($document, $window)
                 y: burger[0].getBoundingClientRect().y
             };
 
-            function getMouseOffset(target, event) {
-                var elementOffset = getPosition(target);
+            element.on('mousedown', mousedown);
+
+            function getMouseOffset(event) {
+                var elementOffset = getElementOffset();
 
                 return {
                     x: event.pageX - elementOffset.x,
@@ -77,41 +83,20 @@ app.directive('draggable', ['$document', '$window', function($document, $window)
                 };
             }
 
-            element.on('mousedown', mousedown);
-
             function mousedown(event) {
                 event.preventDefault();
 
-                mouseOffset = getMouseOffset(element, event);
+                // phoneBorder.classList.remove('is-animate');
+                // burger.classList.remove('is-animate');
+
+                mouseOffset = getMouseOffset(event);
 
                 $document.on('mousemove', mousemove);
                 $document.on('mouseup', mouseup);
                 return false;
             }
             function mousemove(event) {
-                xPos = event.pageX - mouseOffset.x - areaPosition.x;
-                yPos = event.pageY - mouseOffset.y - areaPosition.y;
-
-                if (yPos >= 0 && yPos <= areaPosition.y1) {
-                    newOffset.y = yPos;
-                } else {
-                    if (yPos > areaPosition.y1) {
-                        newOffset.y = areaPosition.y1;
-                    }
-                    else {
-                        newOffset.y = 0;
-                    }
-                }
-                if (xPos >= 0 && xPos <= areaPosition.x1) {
-                    newOffset.x = xPos;
-                } else {
-                    if (xPos > areaPosition.x1) {
-                        newOffset.x = areaPosition.x1;
-                    }
-                    else {
-                        newOffset.x = 0;
-                    }
-                }
+                newOffset = calcPosition(event);
 
                 element.css({
                     top: newOffset.y + 'px',
@@ -126,8 +111,8 @@ app.directive('draggable', ['$document', '$window', function($document, $window)
                     right: 'auto'
                 });
                 burger.css({
-                    top: burgerPosition.y - getElementOffset().y - 80 + 'px',
-                    left: burgerPosition.x - getElementOffset().x - 18 + 'px'
+                    top: burgerPosition.y - getElementOffset().y - 14 + 'px',
+                    left: burgerPosition.x - getElementOffset().x - 14 + 'px'
                 });
 
                 return false;
@@ -141,20 +126,87 @@ app.directive('draggable', ['$document', '$window', function($document, $window)
                 var de = $document[0].documentElement;
                 var box = element[0].getBoundingClientRect();
 
-
                 return {
                     y: box.top + $window.pageYOffset - de.clientTop,
                     x: box.left + $window.pageXOffset - de.clientLeft
                 };
             }
-            function getPosition(elem) {
-                var de = $document[0].documentElement;
-                var box = elem[0].getBoundingClientRect();
+            function calcPosition(elem) {
+                var resultX, resultY;
+                var xPos = elem.pageX - mouseOffset.x - areaPosition.x;
+                var yPos = elem.pageY - mouseOffset.y - areaPosition.y;
+
+                if (yPos >= 0 && yPos <= areaPosition.y1) {
+                    resultY = yPos;
+                } else {
+                    if (yPos > areaPosition.y1) {
+                        resultY = areaPosition.y1;
+                    }
+                    else {
+                        resultY = 0;
+                    }
+                }
+                if (xPos >= 0 && xPos <= areaPosition.x1) {
+                    resultX = xPos;
+                } else {
+                    if (xPos > areaPosition.x1) {
+                        resultX = areaPosition.x1;
+                    }
+                    else {
+                        resultX = 0;
+                    }
+                }
 
                 return {
-                    y: box.top + $window.pageYOffset - de.clientTop,
-                    x: box.left + $window.pageXOffset - de.clientLeft
-                };
+                    x: resultX,
+                    y: resultY
+                }
+            }
+
+            element[0].addEventListener('touchstart', touchstart, false);
+
+            function touchstart(event) {
+                event.preventDefault();
+
+                if (event.targetTouches.length == 1) {
+                    var touch = event.targetTouches[0];
+                    mouseOffset = getMouseOffset(touch);
+
+                    element[0].addEventListener('touchmove', touchmove, false);
+                    element[0].addEventListener('touchend', touchend, false);
+                }
+            }
+            function touchmove(event) {
+                event.preventDefault();
+
+                if (event.targetTouches.length == 1) {
+                    var touch = event.targetTouches[0];
+                    newOffset = calcPosition(touch);
+
+                    element.css({
+                        top: newOffset.y + 'px',
+                        left: newOffset.x + 'px',
+                        bottom: 'auto',
+                        right: 'auto'
+                    });
+                    phoneBorder.css({
+                        top: newOffset.y + 'px',
+                        left: newOffset.x + 'px',
+                        bottom: 'auto',
+                        right: 'auto'
+                    });
+                    burger.css({
+                        top: burgerPosition.y - getElementOffset().y - 14 + 'px',
+                        left: burgerPosition.x - getElementOffset().x - 14 + 'px'
+                    });
+
+                    return false;
+                }
+            }
+
+            function touchend() {
+                element[0].removeEventListener('touchmove', touchmove);
+                element[0].removeEventListener('touchend', touchend);
             }
         }
     };
